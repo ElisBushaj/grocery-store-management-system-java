09423ec868727b64f94577c409b3117f36661390
ElisBushaj <elisbushaj2@gmail.com>
2023-12-13 22:30:53 +0100

    main

--- src/main/java/com/example/demo/GroceryStore.java
+++ src/main/java/com/example/demo/GroceryStore.java
@@ -7,17 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class GroceryStore extends Application {
     public static void main(String[] args) {
         launch();
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GroceryStore.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Grocery Store Management System!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
